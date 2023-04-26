4114845a47cf3ad904937b57fc202ead2f279a4f
noahbaculi <nbaculi@carium.com>
2023-04-26 10:04:26 -0700

    version 0.1.1

--- Formula/busca.rb
+++ Formula/busca.rb
@@ -1,9 +1,9 @@
 class Busca < Formula
-  desc "Closest match searcher written in Rust"
+  desc "Closest match searcher for MacOS written in Rust"
   homepage "https://github.com/noahbaculi/busca"
-  url "https://github.com/noahbaculi/busca/releases/download/v0.1.0/busca-mac.tar.gz"
-  sha256 "d82916d90df20f7ba203fbffe18c70e25b257a4bd03686a211268999a7553399"
-  version "0.1.0"
+  url "https://github.com/noahbaculi/busca/releases/download/v0.1.1/busca-mac.tar.gz"
+  sha256 "4b383b2f91fbf3c07e7177a74684e9f975925a4e1ba598bd2294c1161edda8a9"
+  version "0.1.1"
 
   def install
     bin.install "busca"
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # homebrew-busca
+
 Homebrew Tap for Busca
+
+## Update the Homebrew Tap
+
+Update the `Formula/busca.rb` file:
+
+```ruby
+class Busca < Formula
+  desc "Closest match searcher for MacOS written in Rust"
+  homepage "https://github.com/noahbaculi/busca"
+  url "__link_to_tar_in_the_github_release__"
+  sha256 "__sha_for_tar__"
+  version "0.1.1"
+
+  def install
+    bin.install "busca"
+  end
+end
+```
+
+```shell
+git add .
+git commit -m "version 0.1.1"
+git push
+```
