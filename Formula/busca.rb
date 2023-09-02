39e0883a57a50c37702a364a73feb1c22bcb70f5
noahbaculi <nbaculi@carium.com>
2023-09-02 15:43:16 -0700

    v2.2.1 release

--- Formula/busca.rb
+++ Formula/busca.rb
@@ -1,9 +1,9 @@
 class Busca < Formula
   desc "Closest match searcher for MacOS written in Rust"
   homepage "https://github.com/noahbaculi/busca"
-  url "https://github.com/noahbaculi/busca/releases/download/v1.3.3/busca-mac.tar.gz"
-  sha256 "2cfa82a97125f18f408afd366dbcb68a1373606d6e7cbc5632da7dcaa03eefc8"
-  version "1.3.3"
+  url "https://github.com/noahbaculi/busca/releases/download/v2.2.1/busca-mac.tar.gz"
+  sha256 "c493a04c2e65eda5753f4bd870c8135e98c5cdf9995068b08c5d3b49bffc4dcc"
+  version "2.2.1"
 
   def install
     bin.install "busca"
